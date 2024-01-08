6b70762b3f7fee546b721c9bb44703f8f1783ac4
Valentyn Serediuk <serediukit@gmail.com>
2024-01-08 14:31:04 +0200

    Update index.js

--- lab4/token_auth/index.js
+++ lab4/token_auth/index.js
@@ -11,9 +11,11 @@ const axios = require('axios')
 const CLIENT_ID = 'MaMofmHMvlLkgmcAPiuYoKVANd5G1rPw'
 const CLIENT_SECRET = '7cYPsb7oiXjY27GopxjjEozjwKbxeJFhG3SPXKYzwrkQLSPIijEAshQueWbel53z'
 const URL = 'https://dev-o64lwhkf.us.auth0.com/'
+const TOKEN_URL = URL + 'oauth/token'
 const AUDIENCE = 'https://dev-o64lwhkf.us.auth0.com/api/v2/'
-const GRANT_TYPE = 'http://auth0.com/oauth/grant-type/password-realm'
-const SESSION_KEY = 'Authorization'
+const GRANT_TYPE_PASSWORD_REALM = 'http://auth0.com/oauth/grant-type/password-realm'
+const GRANT_TYPE_CLIENT_CREDENTIALS = 'client_credentials'
+const GRANT_TYPE_REFRESH_TOKEN = 'refresh_token'
 
 const app = express();
 app.use(bodyParser.json());
@@ -50,38 +52,105 @@ app.get('/create-user', (req, res) => {
     return res.sendFile(path.join(__dirname + '/create-user.html'));
 });
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
+app.get('/api/create', async (req, res) => {
+    const { login, password } = req.body
+
+    try {
+        const respUserToken = await axios.post(
+            TOKEN_URL,
+            {
+                audience: AUDIENCE,
+                grant_type: GRANT_TYPE_CLIENT_CREDENTIALS,
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+
+        const respRefreshToken = await axios.post(
+            `${AUDIENCE}users`,
+            {
+                connection: 'Username-Password-Authentication',
+                email: login,
+                password: password
+            },
+            {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${respUserToken.data.access_token}`
+            }
+        )
+
+        res.status(201).json({
+            message: 'User created'
+        })
+    } catch (err) {
+        res.status(401).json({
+            error: err.response.data
+        })
     }
-]
+})
+
+app.get('/api/refresh', async (req, res) => {
+    const { refresh_token } = req.body
+
+    try {
+        const resp = await axios.post(
+            TOKEN_URL,
+            {
+                audience: AUDIENCE,
+                grant_type: GRANT_TYPE_REFRESH_TOKEN,
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                scope: 'offline_access',
+                realm: 'Username-Password-Authentication',
+                refresh_token: refresh_token
+            },
+            {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        )
+
+        res.json({
+            access_token: resp.data.access_token,
+            refresh_token: resp.data.refresh_token
+        })
+    } catch (err) {
+        res.status(401).json({
+            error: err.response.data
+        })
+    }
+})
 
 app.post('/api/login', async (req, res) => {
     const { login, password } = req.body;
 
     try {
-        const resp = await axios.post('$oauth/token', {
-            audience: AUDIENCE,
-            grant_type: GRANT_TYPE,
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            scope: 'offline_access',
-            realm: 'Username-Password-Authentication',
-            username: login,
-            password: password
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
+        const resp = await axios.post(
+            TOKEN_URL,
+            {
+                audience: AUDIENCE,
+                grant_type: GRANT_TYPE_PASSWORD_REALM,
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                scope: 'offline_access',
+                realm: 'Username-Password-Authentication',
+                username: login,
+                password: password
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
             }
-        })
+        )
         res.status(201).json({
             access_token: resp.data.access_token,
             refresh_token: resp.data.refresh_token,
@@ -93,6 +162,8 @@ app.post('/api/login', async (req, res) => {
         })
     }
 });
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
