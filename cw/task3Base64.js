0389df854e5b5cbf6bc7cb6fe4eef451ba935bed
Valentyn Serediuk <serediukit@gmail.com>
2024-01-09 15:12:07 +0200

    Create task3Base64.js

---
+++ cw/task3Base64.js
@@ -0,0 +1,7 @@
+data = '0222, valentyn serediuk, serediukit@gmail.com'
+
+const encodedData = btoa(data);
+console.log("Encoded: ", encodedData);
+
+const decodedData = atob(encodedData);
+console.log("Decoded: ", decodedData);
